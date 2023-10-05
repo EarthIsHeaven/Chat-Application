2b81fd1a6c919866744a78978a86a41fd2e5d601
EarthIsHeaven <thinleylama44@gmail.com>
2023-10-05 17:24:56 +0530

    Initialized socket.io

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 import express from 'express';
 import { createServer } from 'node:http';
+import { Server } from 'socket.io';
+
+// import { dirname } from "path";
+// import { fileURLToPath } from 'node:url';
+// const __dirname = dirname(fileURLToPath(import.meta.url));
+
 
 const app = express();
 const server = createServer(app);
+const io = new Server(server);
+
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname +"/index.html");
+// })
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello world</h1>');
+  res.sendFile(new URL('./index.html', import.meta.url).pathname);
+});
+
+io.on('connection', (socket)=> {
+    console.log('a user connected');
+    socket.on('disconnect', ()=> {
+        console.log('user disconnected');
+    })
 })
 
 server.listen(3000, () => {
-    console.log("Server running on port 3000");
-})
+  console.log('server running at port 3000');
+});
